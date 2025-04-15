18e2e3d5a2fcb5adc3739588ddd5b28d51fd89ee
varshan4068 <j42830963@gmail.com>
2025-04-15 11:43:52 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,374 @@
+#!/usr/bin/env bash
+
+print_logo(){
+	cat << "EOF"
+ __ __  __ __  ____  ____   _       ____  ____   ___        ___     ___   ______  _____  ____  _        ___  _____
+|  |  ||  |  ||    \|    \ | |     /    ||    \ |   \      |   \   /   \ |      ||     ||    || |      /  _]/ ___/
+|  |  ||  |  ||  o  )  D  )| |    |  o  ||  _  ||    \     |    \ |     ||      ||   __| |  | | |     /  [_(   \_ 
+|  _  ||  ~  ||   _/|    / | |___ |     ||  |  ||  D  |    |  D  ||  O  ||_|  |_||  |_   |  | | |___ |    _]\__  |
+|  |  ||___, ||  |  |    \ |     ||  _  ||  |  ||     |    |     ||     |  |  |  |   _]  |  | |     ||   [_ /  \ |
+|  |  ||     ||  |  |  .  \|     ||  |  ||  |  ||     |    |     ||     |  |  |  |  |    |  | |     ||     |\    |
+|__|__||____/ |__|  |__|\_||_____||__|__||__|__||_____|    |_____| \___/   |__|  |__|   |____||_____||_____| \___|
+
+EOF
+}
+
+clear
+print_logo
+
+set -e
+
+echo "Installation has been started "
+
+dependencies=(
+
+	git
+	neovim
+	curl
+	wget
+	tmux
+	base-devel
+	gcc
+	make
+	python
+	npm
+	go
+	fzf
+	vscode
+	waybar
+	rofi-wayland
+	ufw
+	tldr
+	timeshift
+	thunar
+	thermald 
+	swappy
+	papirus-icon-theme
+	kdeconnect
+	nwg-displays
+	nwg-look
+	hyprpaper
+	hyprlock
+	grub-btrfs
+	bash-completion
+	libreoffice-fresh
+	brightnessctl
+	acpi
+	capitaine-cursors
+	xorg-xhost
+	tree
+	qalculate-gtk
+	gvfs-mtp
+	evolution
+	gimp
+	kdenlive
+	mission-center
+	gnome-boxes
+	telegram-desktop
+	transmission-gtk
+	filelight
+	impression
+
+)
+
+yay=(
+
+	zen-browser-bin
+	dracula-gtk-theme
+	hyprshot
+	timeshift-autosnap
+	battop
+)
+
+read -p "Do u wanna install the dependencies? (y/n): " package
+
+case "$package" in
+
+	y)
+		echo "Installing dependencies"
+
+		sudo pacman -S "${dependencies[@]}"
+		;;
+	n)
+		clear
+		;;
+esac
+
+
+
+yay_install(){
+	cat << EOF
+
+
+  ██╗   ██╗ █████╗ ██╗   ██╗
+  ╚██╗ ██╔╝██╔══██╗╚██╗ ██╔╝
+   ╚████╔╝ ███████║ ╚████╔╝ 
+    ╚██╔╝  ██╔══██║  ╚██╔╝  
+     ██║   ██║  ██║   ██║   
+     ╚═╝   ╚═╝  ╚═╝   ╚═╝   
+																									 
+
+EOF
+}
+
+yay_install
+
+	
+read -p "Do u wanna install yay aur helper? (y/n): " aur
+	
+case "$aur" in
+	y)
+		echo "Installing yay ( AUR helper )"
+
+		git clone https://aur.archlinux.org/yay.git ~/yay
+		cd yay
+		makepkg -si 
+		rm -r ~/yay/
+		;;
+	n)
+		clear
+		;;
+esac
+
+read -p "Do u wanna install yay dependencies? (y/n): " depen
+
+case "$depen" in
+	y)
+		echo "Installing yay dependencies"
+
+		yay -S "${yay[@]}"
+		;;
+	n)
+		clear
+esac
+
+cpu(){
+	cat << "EOF"
+
+  ____  __ __  ______   ___          __  ____  __ __  _____  ____     ___   ___  
+ /    ||  |  ||      | /   \        /  ]|    \|  |  ||     ||    \   /  _] /   \ 
+|  o  ||  |  ||      ||     |      /  / |  o  )  |  ||   __||  D  ) /  [_ |     |
+|     ||  |  ||_|  |_||  O  |     /  /  |   _/|  |  ||  |_  |    / |    _]|  Q  |
+|  _  ||  :  |  |  |  |     |    /   \_ |  |  |  :  ||   _] |    \ |   [_ |     |
+|  |  ||     |  |  |  |     |    \     ||  |  |     ||  |   |  .  \|     ||     |
+|__|__| \__,_|  |__|   \___/      \____||__|   \__,_||__|   |__|\_||_____| \__,_|
+																																					 
+
+EOF
+}
+
+cpu
+
+read -p "Do u wanna install auto-cpufreq for power saving? (y/n): " cpu 
+
+case "$cpu" in 
+	y)
+
+		git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+		cd auto-cpufreq &&  sudo ./auto-cpufreq-installer
+		sudo auto-cpufreq --install
+		;;
+	n)
+		clear 
+		;;
+esac
+
+files(){
+	cat << "EOF"
+
+	 ___     ___   ______  _____  ____  _        ___  _____
+	|   \   /   \ |      ||     ||    || |      /  _]/ ___/
+	|    \ |     ||      ||   __| |  | | |     /  [_(   \_ 
+	|  D  ||  O  ||_|  |_||  |_   |  | | |___ |    _]\__  |
+	|     ||     |  |  |  |   _]  |  | |     ||   [_ /  \ |
+	|     ||     |  |  |  |  |    |  | |     ||     |\    |
+	|_____| \___/   |__|  |__|   |____||_____||_____| \___|
+																												 
+
+EOF
+}
+
+files
+
+read -p "Do u wanna install Dotfiles and some other files? (y/n): " dot
+
+case "$dot" in 
+y)
+
+	cd ~/.dotfiles/
+	cp starship.toml  nvim/ kitty/ hypr/ dunst/ backgrounds/ rofi/ waybar/  ~/.config/
+	cp .bashrc  rofi-wifi-menu/ .icons/   Top-5-Bootloader-Themes/ ~
+	sudo cp rofi-power-menu /usr/bin/
+	cd ~
+	;;
+n)
+	clear
+esac
+
+anime(){
+	cat << EOF
+
+				 █████╗ ███╗   ██╗██╗       ██████╗██╗     ██╗
+				██╔══██╗████╗  ██║██║      ██╔════╝██║     ██║
+				███████║██╔██╗ ██║██║█████╗██║     ██║     ██║
+				██╔══██║██║╚██╗██║██║╚════╝██║     ██║     ██║
+				██║  ██║██║ ╚████║██║      ╚██████╗███████╗██║
+				╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝       ╚═════╝╚══════╝╚═╝
+
+
+EOF
+}
+
+anime
+
+read -p "Do you wanna Install ani-cli? (y/n): " ani
+
+case "$ani" in
+
+y )
+	echo "Setting Up ani-cli"
+
+	git clone "https://github.com/pystardust/ani-cli.git"
+	cp ani-cli/ani-cli /usr/local/bin
+	rm -rf ani-cli
+	;;
+n )
+	clear
+	;;
+esac
+
+
+ufw(){
+	cat << "EOF"
+		 __ __  _____  __    __ 
+		|  |  ||     ||  |__|  |
+		|  |  ||   __||  |  |  |
+		|  |  ||  |_  |  |  |  |
+		|  :  ||   _] |  `  '  |
+		|     ||  |    \      / 
+		 \__,_||__|     \_/\_/  
+														
+EOF
+}
+
+ufw
+
+sudo ufw enable
+sudo ufw status
+sudo ufw limit 22/tcp  
+sudo ufw allow 80/tcp  
+sudo ufw allow 443/tcp  
+sudo ufw default deny incoming  
+sudo ufw default allow outgoing
+
+boot(){
+	cat << "EOF"
+
+
+
+ ████████╗██╗  ██╗███████╗███╗   ███╗███████╗
+ ╚══██╔══╝██║  ██║██╔════╝████╗ ████║██╔════╝
+    ██║   ███████║█████╗  ██╔████╔██║█████╗  
+    ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══╝  
+    ██║   ██║  ██║███████╗██║ ╚═╝ ██║███████╗
+    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝
+
+
+EOF
+}
+
+clear
+boot
+
+cd ~/.dotfiles/Top-5-Bootloader-Themes/
+sudo ./install.sh ## then select the theme u wanted u install 
+cd ~
+
+sddm(){
+	cat << "EOF"
+
+	███████╗██████╗ ██████╗ ███╗   ███╗    ████████╗██╗  ██╗███████╗███╗   ███╗███████╗███████╗
+	██╔════╝██╔══██╗██╔══██╗████╗ ████║    ╚══██╔══╝██║  ██║██╔════╝████╗ ████║██╔════╝██╔════╝
+	███████╗██║  ██║██║  ██║██╔████╔██║       ██║   ███████║█████╗  ██╔████╔██║█████╗  ███████╗
+	╚════██║██║  ██║██║  ██║██║╚██╔╝██║       ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══╝  ╚════██║
+	███████║██████╔╝██████╔╝██║ ╚═╝ ██║       ██║   ██║  ██║███████╗██║ ╚═╝ ██║███████╗███████║
+	╚══════╝╚═════╝ ╚═════╝ ╚═╝     ╚═╝       ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝╚══════╝
+
+
+EOF
+}
+
+clear
+sddm
+
+read -p "Do u wanna install sddm themes? (y/n): " lm
+
+case "$lm" in
+y)
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+  ;;
+n)
+	echo "Everything is Installed perfectly just reboot your system to get perfect results"
+	sleep 3
+	clear
+	;;
+esac
+
+reboot(){
+	cat << "EOF"
+
+	██████╗ ███████╗██████╗  ██████╗  ██████╗ ████████╗
+	██╔══██╗██╔════╝██╔══██╗██╔═══██╗██╔═══██╗╚══██╔══╝
+	██████╔╝█████╗  ██████╔╝██║   ██║██║   ██║   ██║   
+	██╔══██╗██╔══╝  ██╔══██╗██║   ██║██║   ██║   ██║   
+	██║  ██║███████╗██████╔╝╚██████╔╝╚██████╔╝   ██║   
+	╚═╝  ╚═╝╚══════╝╚═════╝  ╚═════╝  ╚═════╝    ╚═╝   
+																										 
+EOF
+}
+
+reboot
+
+read -p "Do u wanna reboot ur system? (y/n): " choose
+
+case "$choose" in
+	y )
+		echo "Rebooting ur system now....."
+		systemctl reboot
+		sleep 3
+		;;
+	n )
+		clear
+		;;
+esac
+
+shutdown(){
+	cat << "EOF"
+
+	 ███████╗██╗  ██╗██╗   ██╗████████╗██████╗  ██████╗ ██╗    ██╗███╗   ██╗
+	 ██╔════╝██║  ██║██║   ██║╚══██╔══╝██╔══██╗██╔═══██╗██║    ██║████╗  ██║
+	 ███████╗███████║██║   ██║   ██║   ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║
+	 ╚════██║██╔══██║██║   ██║   ██║   ██║  ██║██║   ██║██║███╗██║██║╚██╗██║
+	 ███████║██║  ██║╚██████╔╝   ██║   ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║
+	 ╚══════╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝
+																																					
+EOF
+}
+
+shutdown
+
+read -p "Do u wanna Shutdown ur system? (y/n): " select
+
+case "$select" in 
+	y )
+		echo "Shutdowing ur system now....."
+		systemctl poweroff 
+		;;
+	n )
+		clear
+		exit 0
+		;;
+esac
+
+
+
